6a6257e5dcca052a375eb83962664a6347955e0d
Brig Young <brig@sonophotostudios.com>
2017-03-25 21:15:45 -0700

    Fixed bonked zip command in build_library.sh

--- build_library.sh
+++ build_library.sh
@@ -1,9 +1,8 @@
 echo    "************************************************************************************"
 echo -e "Building library zip file for Mega2560_Ports\n"
 
-
-zip -rm lib-out/Mega2560_Ports.zip examples/*
-zip -rm lib-out/Mega2560_Ports.zip src/*
-zip -rm lib-out/Mega2560_Ports.zip tests/*
-zip -rm lib-out/Mega2560_Ports.zip schematics/*
-zip -m  lib-out/Mega2560_Ports.zip library.properties keywords.txt README.md LICENSE
+zip lib-out/Mega2560_Ports.zip library.properties keywords.txt README.md LICENSE
+zip -r lib-out/Mega2560_Ports.zip src/*
+zip -r lib-out/Mega2560_Ports.zip tests/*
+zip -r lib-out/Mega2560_Ports.zip examples/*
+zip -r lib-out/Mega2560_Ports.zip schematics/*
