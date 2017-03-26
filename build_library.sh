d24c56032e0df28b80658cdfea689dd399122135
Brig Young <brig@sonophotostudios.com>
2017-03-25 22:59:55 -0700

    Tightened up the build script

--- build_library.sh
+++ build_library.sh
@@ -1,8 +1,25 @@
 echo    "************************************************************************************"
 echo -e "Building library zip file for Mega2560_Ports\n"
 
-zip lib-out/Mega2560_Ports.zip library.properties keywords.txt README.md LICENSE
-zip -r lib-out/Mega2560_Ports.zip src/*
-zip -r lib-out/Mega2560_Ports.zip tests/*
-zip -r lib-out/Mega2560_Ports.zip examples/*
-zip -r lib-out/Mega2560_Ports.zip schematics/*
+#update our version
+VERSION=`cat VERSION`
+
+#update our filename
+ZIPNAME=Mega2560_Ports-$VERSION.zip
+echo "Filename is: " $ZIPNAME
+echo ""
+
+#Checking for zip
+zip --version > /dev/null 2>&1
+if [ $? == 0 ]
+then
+   zip    lib-out/$ZIPNAME library.properties keywords.txt README.md LICENSE
+   zip -r lib-out/$ZIPNAME src/*
+   zip -r lib-out/$ZIPNAME tests/*
+   zip -r lib-out/$ZIPNAME examples/*
+   zip -r lib-out/$ZIPNAME schematics/*
+   
+fi
+
+
+
