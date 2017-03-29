55615fcfb7d263f1d115135cfaa729a75998386d
Brig Young <brig@sonophotostudios.com>
2017-03-29 16:16:55 -0700

    Reorganizing examples directory

--- build_library.sh
+++ build_library.sh
@@ -13,11 +13,14 @@ echo ""
 zip --version > /dev/null 2>&1
 if [ $? == 0 ]
 then
+   echo "Zipping base directory of the project..."
    zip    lib-out/$ZIPNAME library.properties keywords.txt README.md LICENSE
-   zip -r lib-out/$ZIPNAME src/*
-   zip -r lib-out/$ZIPNAME tests/*
+   echo "Zipping src files for the library..."
+   zip -r lib-out/$ZIPNAME src/*.h src/*.c src/*.hpp src/*.cpp 
+   echo "Zipping test system for the library..."
+   zip -r lib-out/$ZIPNAME tests/*.ino tests/*.h tests/*.c tests/*.hpp tests/*.cpp 
+   echo "Zipping examples directory..."
    zip -r lib-out/$ZIPNAME examples/*
-   zip -r lib-out/$ZIPNAME schematics/*
    
 fi
 
